1479d2f4953ac6486bb6c7afdca956f00b3a93d1
AhmedMedhat123 <117312363+AhmedMedhat123@users.noreply.github.com>
2024-11-30 23:24:49 +0200

    player

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-
 import Sidebar from "./components/Sidebar"
+import Player from "./components/Player"
 function App() {
 
   return (
     <>
-      <div className="h-screen bg-black overflow-hidden">
-        <div className="p-3">
+      <div className="h-screen bg-black">
+        <div className='h-screen text-white p-3'>
           <Sidebar />
+          <Player />
         </div>
       </div>
     </>
