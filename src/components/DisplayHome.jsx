9620e466efbd5d0a30ecc1db1d65e7b2afe8d84f
AhmedMedhat123 <117312363+AhmedMedhat123@users.noreply.github.com>
2024-12-01 21:08:56 +0200

    songs

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,9 +1,24 @@
 import Navbar from "./Navbar"
-
+import AlbumItem from "./AlbumItem"
+import SongItem from "./SongItem"
+import { albumsData, songsData } from '../assets/assets'
 const DisplayHome = () => {
     return (
         <>
             <Navbar />
+            <h1 className="my-4 font-bold text-2xl">Featured Charts</h1>
+            <div className="flex gap-3 overflow-auto">
+                {albumsData.map((item, index) => (
+                    <AlbumItem key={index} id={item.id} name={item.name} image={item.image} desc={item.desc} />
+                ))}
+            </div>
+            <h1 className="my-4 font-bold text-2xl">Today's biggest hits</h1>
+            <div className="flex gap-3 overflow-auto">
+                {songsData.map((item, index) => (
+                    <SongItem key={index} id={item.id} name={item.name} image={item.image} desc={item.desc} />
+                ))}
+            </div>
+
         </>
     )
 }
---
+++ src/components/SongItem.jsx
@@ -0,0 +1,12 @@
+
+const SongItem = ({ id, name, image, desc }) => {
+    return (
+        <div className="card">
+            <img src={image} alt="" className="rounded w-40" />
+            <p className="font-bold mt-2 mb-1" >{name}</p>
+            <p className="text-slate-200 text-sm">{desc}</p>
+        </div>
+    )
+}
+
+export default SongItem
