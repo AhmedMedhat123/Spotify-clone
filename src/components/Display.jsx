7da39b38255813f29020ebc8ce9ebf6ed4bee54d
Abdelrahman Mohamed <108595646+Vayne-1@users.noreply.github.com>
2024-12-02 23:06:43 +0200

    display song

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,33 +1,38 @@
-import React,{useEffect, useRef} from "react"
-import { Routes, Route,useLocation } from "react-router-dom"
-import DisplayHome from "./DisplayHome"
-import DisplayAlbum from "./DisplayAlbum"
-import { albumsData } from "../assets/assets"
+import React, { useEffect, useRef } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import DisplayHome from "./DisplayHome";
+import DisplayAlbum from "./DisplayAlbum";
+import DisplaySong from "./DisplaySong";
+import { albumsData } from "../assets/assets";
 const Display = () => {
-    const displayRef = useRef();
-    const location = useLocation();
-    const isAlbum = location.pathname.includes("album");
-    const albumId = isAlbum ? location.pathname.slice(-1):"";
-    const bgColor= albumsData[Number(albumId)].bgColor;
+  const displayRef = useRef();
+  const location = useLocation();
+  const isAlbum = location.pathname.includes("album");
+  const albumId = isAlbum ? location.pathname.slice(-1) : "";
+  const bgColor = albumsData[Number(albumId)].bgColor;
 
-    useEffect(()=>{
-        if(isAlbum){
-            displayRef.current.style.background=`linear-gradient(${bgColor},#121212)`;
-        }else{
-            displayRef.current.style.background=`#121212`;  
-        }
-    })
+  useEffect(() => {
+    if (isAlbum) {
+      displayRef.current.style.background = `linear-gradient(${bgColor},#121212)`;
+    } else {
+      displayRef.current.style.background = `#121212`;
+    }
+  });
 
-    return (
-        <>
-            <div ref={displayRef} className="w-[100%] max-h-[39.3rem] bg-[#121212] rounded-md px-5 py-3 ml-2 overflow-auto">
-                <Routes>
-                    <Route path='/' element={<DisplayHome />}></Route>
-                    <Route path='/album/:id' element={<DisplayAlbum />}></Route>
-                </Routes>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div
+        ref={displayRef}
+        className="w-[100%] max-h-[39.3rem] bg-[#121212] rounded-md px-5 py-3 ml-2 overflow-auto"
+      >
+        <Routes>
+          <Route path="/" element={<DisplayHome />}></Route>
+          <Route path="/album/:id" element={<DisplayAlbum />}></Route>
+          <Route path="/song/:id" element={<DisplaySong />}></Route>
+        </Routes>
+      </div>
+    </>
+  );
+};
 
-export default Display
+export default Display;
--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,12 +1,14 @@
+import { useNavigate } from "react-router-dom";
 
 const SongItem = ({ id, name, image, desc }) => {
-    return (
-        <div className="card">
-            <img src={image} alt="" className="rounded w-40" />
-            <p className="font-bold mt-2 mb-1" >{name}</p>
-            <p className="text-slate-200 text-sm">{desc}</p>
-        </div>
-    )
-}
+  const navigate = useNavigate();
+  return (
+    <div onClick={() => navigate(`/song/${id}`)} className="card">
+      <img src={image} alt="" className="rounded w-40" />
+      <p className="font-bold mt-2 mb-1">{name}</p>
+      <p className="text-slate-200 text-sm">{desc}</p>
+    </div>
+  );
+};
 
-export default SongItem
+export default SongItem;
