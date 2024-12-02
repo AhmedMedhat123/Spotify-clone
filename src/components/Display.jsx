95fc17feb34c9917aaebbfe0fd87ecccc566260d
AhmedMedhat123 <117312363+AhmedMedhat123@users.noreply.github.com>
2024-12-02 19:14:29 +0200

    change-logo

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,5 @@
-import React,{useEffect, useRef} from "react"
-import { Routes, Route,useLocation } from "react-router-dom"
+import React, { useEffect, useRef } from "react"
+import { Routes, Route, useLocation } from "react-router-dom"
 import DisplayHome from "./DisplayHome"
 import DisplayAlbum from "./DisplayAlbum"
 import { albumsData } from "../assets/assets"
@@ -7,14 +7,14 @@ const Display = () => {
     const displayRef = useRef();
     const location = useLocation();
     const isAlbum = location.pathname.includes("album");
-    const albumId = isAlbum ? location.pathname.slice(-1):"";
-    const bgColor= albumsData[Number(albumId)].bgColor;
+    const albumId = isAlbum ? location.pathname.slice(-1) : "";
+    const bgColor = albumsData[Number(albumId)].bgColor;
 
-    useEffect(()=>{
-        if(isAlbum){
-            displayRef.current.style.background=`linear-gradient(${bgColor},#121212)`;
-        }else{
-            displayRef.current.style.background=`#121212`;  
+    useEffect(() => {
+        if (isAlbum) {
+            displayRef.current.style.background = `linear-gradient(${bgColor},#121212)`;
+        } else {
+            displayRef.current.style.background = `#121212`;
         }
     })
 
