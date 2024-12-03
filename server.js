80a841cb6b736704fbe7693508407a13d515bf27
Osama-medhat0 <ososmedhat1@gmail.com>
2024-12-04 00:27:22 +0200

    setup backend

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT || 1000;
+
+app.use(cors()); //to connect back-end with fornt-end
+app.use(express.json());
+
+//testing API
+app.get("/", (req, res) => res.send("working"));
+app.listen(port, () => console.log(`Server working on ${port}`)); //starting express app
