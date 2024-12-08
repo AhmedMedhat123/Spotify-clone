e979e9c7962a3ed25fb7f06cade822efa634aaa1
Osama-medhat0 <ososmedhat1@gmail.com>
2024-12-08 20:35:33 +0200

    Add server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-import express from "express";
+import express from "express"; // to create a server
 import cors from "cors";
 import "dotenv/config";
+import DB from "./src/config/mongoDB.js";
+import songRouter from "./src/routes/songRoutes.js";
 
 const app = express();
+DB();
 const port = process.env.PORT || 1000;
 
 app.use(cors()); //to connect back-end with fornt-end
@@ -11,3 +14,6 @@ app.use(express.json());
 //testing API
 app.get("/", (req, res) => res.send("working"));
 app.listen(port, () => console.log(`Server working on ${port}`)); //starting express app
+
+//starting Routes
+app.use("/song", songRouter); //error /api
