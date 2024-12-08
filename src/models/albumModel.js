89069fc72166103819f0503028d6e93250125a1b
Osama-medhat0 <ososmedhat1@gmail.com>
2024-12-08 21:16:07 +0200

    album model

---
+++ src/models/albumModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const albumSchema = new mongoose.Schema({
+  album_name: { type: String, required: true },
+  description: { type: String, required: true },
+  album_image: { type: String, required: true },
+  background_color: { type: String, required: true },
+});
+
+const songModel = mongoose.models.song || mongoose.model("song", albumSchema); //check if thier is a songs collection in the database if ! make a model ( model represents the songs collection )
+
+export default songModel;
