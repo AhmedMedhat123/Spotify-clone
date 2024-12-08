f655f24868c0b6b117aa21087a3d666bfd6dbcea
Osama-medhat0 <ososmedhat1@gmail.com>
2024-12-08 21:04:57 +0200

    song model file

--- src/models/songModel.js
+++ src/models/songModel.js
@@ -8,4 +8,6 @@ const schema = new mongoose.Schema({
   file: { type: String, required: true }, //error
 });
 
-const songModel = mongoose.models.song || mongoose.model("song", songSchema);
+const songModel = mongoose.models.song || mongoose.model("song", schema); //check if thier is a songs collection in the database if ! make a model ( model represents the songs collection )
+
+export default songModel;
