bde189c9bae55d5c5e8757dc6e21b5bd5bd87316
AhmedMedhat123 <117312363+AhmedMedhat123@users.noreply.github.com>
2024-12-10 20:09:57 +0200

    fixing folders

--- spotify-backend/src/config/cloud.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-const connectToCloud = async () => {
-  await cloudinary.config({
-    cloudName: process.env.CLOUD_NAME,
-    apiKey: process.env.API_KEY,
-    apiSecret: process.env.apiSecret,
-  });
-};
-export default connectToCloud;
